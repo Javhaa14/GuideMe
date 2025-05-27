309444090a4a06cbdc644fb2a43e4130cde0fa39
zayabdmr <zayabudmir@gmail.com>
2025-05-27 17:00:28 +0800

    sign-up, log-in okk

--- front/app/(auth)/layout.tsx
+++ front/app/(auth)/layout.tsx
@@ -7,12 +7,10 @@ export default function AuthLayout({
 }) {
   return (
     <div className="flex h-screen w-screen">
-      {/* Left Layout with Background */}
       <div className="flex w-full md:w-1/2">
         <LeftLayout />
       </div>
 
-      {/* Right Content */}
       <div className="flex w-full md:w-1/2 items-center justify-center bg-white">
         {children}
       </div>
