799a785c3c2a7f350299575594ef275372b74f06
24LP1634 24LP1634 <24LP1634@G302-CC0108.local>
2025-05-27 12:47:29 +0800

    haragdah baidal

--- front/app/(main)/Guidesinfo/page.tsx
+++ front/app/(main)/Guidesinfo/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Filter } from "./components/Filter";
+import { GuideProfile } from "./components/GuideProfile";
 
 export default function Home() {
   const filters = [
@@ -11,15 +12,65 @@ export default function Home() {
     "Experienced",
     "Location",
   ];
-
+  const guides = [
+    {
+      name: "Javhaa",
+      location: "Ulaanbaatar",
+      about: "Hiiiiiii",
+      price: 10,
+      rating: 4.5,
+    },
+    {
+      name: "Javhaa",
+      location: "Ulaanbaatar",
+      about: "Hiiiiiii",
+      price: 10,
+      rating: 4.5,
+    },
+    {
+      name: "Javhaa",
+      location: "Ulaanbaatar",
+      about: "Hiiiiiii",
+      price: 10,
+      rating: 4.5,
+    },
+    {
+      name: "Javhaa",
+      location: "Ulaanbaatar",
+      about: "Hiiiiiii",
+      price: 10,
+      rating: 4.5,
+    },
+    { name: "Cat", location: "Madrid", about: "lol", price: 90, rating: 5 },
+    {
+      name: "Javhaa",
+      location: "Ulaanbaatar",
+      about: "Hiiiiiii",
+      price: 10,
+      rating: 4.5,
+    },
+  ];
   return (
-    <div className="flex w-screen h-screen justify-center bg-white pt-[40px] px-[20px]">
+    <div className="flex flex-col w-screen h-screen items-center bg-white gap-10 pt-[40px] px-[20px]">
       <div className="flex border-black border-[3px] gap-4 w-fit h-fit rounded-md p-4">
         {filters.map((v, i) => {
           return <Filter key={i} name={v} />;
         })}
       </div>
-      <div className="grid grid-cols-2 gap-10"></div>
+      <div className="grid grid-cols-2 gap-10 w-full h-fit">
+        {guides.map((v, i) => {
+          return (
+            <GuideProfile
+              rating={v.rating}
+              price={v.price}
+              key={i}
+              name={v.name}
+              location={v.location}
+              about={v.about}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
