15929fc06a1804c6aaa7e3528d14b7e1b6312a64
Javhaa14 <javhaa1410@gmail.com>
2025-05-27 21:53:47 +0800

    profile

---
+++ front/app/(main)/guideProfile/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Profile } from './components/Profile';
+
+const page = () => {
+  return (
+    <div className="flex justify-center px-[200px] py-[50px]">
+      {' '}
+      <Profile />
+    </div>
+  );
+};
+
+export default page;
