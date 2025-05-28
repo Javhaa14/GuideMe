68b1b19872fd0837d9c1336d641c2cf536e7dfaf
24LP1634 24LP1634 <24LP1634@G302-CC0108.local>
2025-05-28 10:21:44 +0800

    main

--- front/app/(main)/page.tsx
+++ front/app/(main)/page.tsx
@@ -1,3 +1,17 @@
+import { Footer } from "./components/Footer";
+import { Guides } from "./components/Guides";
+import { ImageCarousel } from "./components/ImageCarousel";
+import { Navigation } from "./components/Navigation";
+import { Videos } from "./components/Videos";
+
 export default function Home() {
-  return <div>hi</div>;
+  return (
+    <div className="flex flex-col">
+      <Navigation />
+      <ImageCarousel />
+      <Guides />
+      <Videos />
+      <Footer />
+    </div>
+  );
 }
