265183a03a524c19ab019f3d8c5affe8e18cd75b
24LP1634 24LP1634 <24LP1634@G302-CC0108.local>
2025-05-28 19:21:40 +0800

    qr example

--- front/app/(main)/page.tsx
+++ front/app/(main)/page.tsx
@@ -9,32 +9,6 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Home() {
-  const [qr, setQr] = useState("");
-  const [status, setStatus] = useState("");
-  const [paymentId, setPaymentId] = useState(null);
-  const qrgenerate = async () => {
-    setStatus("");
-    const data = await axios.get(`http://localhost:9999`);
-    setQr(data.data.qr);
-    setPaymentId(data.data.id);
-  };
-
-  useEffect(() => {
-    if (!paymentId) return;
-
-    const ws = new WebSocket("ws://localhost:9999");
-    ws.onopen = () => {
-      ws.send(JSON.stringify({ type: "watch", paymentId }));
-    };
-    ws.onmessage = (event) => {
-      const message = JSON.parse(event.data);
-      if (message.status === true) {
-        setStatus("payment success");
-        ws.close();
-      }
-    };
-  }, [paymentId]);
-
   return (
     <div className="flex flex-col">
       <Navigation />
@@ -42,9 +16,6 @@ export default function Home() {
       <Guides />
       <Videos />
 
-      <button onClick={qrgenerate}>pay</button>
-      {qr && <img className="size-[100px]" src={qr} alt="qr"></img>}
-      {status && <p className="text-red-400">Payment success</p>}
       <Footer />
     </div>
   );
