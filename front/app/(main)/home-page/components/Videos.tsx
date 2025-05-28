bc7866fda3cb8c7d5c94ee8b9cde58feb7fa6702
zayabudmir <zayabudmir@gmail.com>
2025-05-28 09:51:14 +0800

    home

--- front/app/(main)/home-page/components/Videos.tsx
+++ front/app/(main)/home-page/components/Videos.tsx
@@ -5,15 +5,17 @@ export const Videos = () => {
     <div className="p-8">
       <h2 className="text-4xl font-bold text-gray-800 mb-10">🌟 Video</h2>
 
-      <div className="w-full h-full mx-auto border rounded overflow-hidden">
-        <video
-          className="w-full h-full object-cover"
-          controls
-          poster="/video.png"
-        >
-          <source src="/video.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+      <div className="w-full h-[900px] mx-auto border rounded overflow-hidden">
+        <iframe
+          width="1800px"
+          height="900px"
+          src="https://www.youtube.com/embed/N_UahGd75EY?si=5Oe_Ge32Qq3VoQQ_"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
       </div>
     </div>
   );
