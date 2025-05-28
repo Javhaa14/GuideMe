a2558336d4cbc73caee3687963878a76f21d3406
24LP1634 24LP1634 <24LP1634@G302-CC0108.local>
2025-05-28 18:55:17 +0800

    main changed

---
+++ front/app/(main)/Guidedetail/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import GuideMainProfile from "../components/GuideMainProfile";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <GuideMainProfile />
+    </div>
+  );
+}
