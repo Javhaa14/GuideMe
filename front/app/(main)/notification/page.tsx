c7b4837509c8014a1974fbbb4408ebb033b15858
24LP7422 24LP7422 <24LP7422@setups-iMac-3.local>
2025-05-28 12:12:14 +0800

    guidelist

---
+++ front/app/(main)/notification/page.tsx
@@ -0,0 +1,11 @@
+import { CheckedGuides } from "@/components/checkedGuides";
+
+const TravelersPost = () => {
+  return (
+    <div>
+      <CheckedGuides />
+    </div>
+  );
+};
+
+export default TravelersPost;
---
+++ front/components/checkedGuides.tsx
@@ -0,0 +1,49 @@
+import { Filter } from "lucide-react";
+import { FilterButton } from "./filterButton";
+import { GuideListInPost } from "./guideListInPost";
+
+const mocData = [
+  {
+    profileName: "guide1",
+    profileImage: "/profileImage.webp",
+    experience: "about",
+    review: "* * * *",
+    chargestatus: "free",
+    facebook: "host:://",
+    instagram: "host:://",
+  },
+  {
+    profileName: "guide2",
+    profileImage: "/profileImage.webp",
+    experience: "about",
+    review: "* * *",
+    chargestatus: "paid",
+    facebook: "host:://",
+    instagram: "host:://",
+  },
+];
+
+export const CheckedGuides = () => {
+  return (
+    <div className="flex flex-col gap-5 pl-[80px] pt-[80px]">
+      <div className="flex gap-5">
+        <FilterButton />
+      </div>
+      <div className="flex flex-col gap-3">
+        {mocData?.map((guide, index) => (
+          <div key={index}>
+            <GuideListInPost
+              profileImage={guide.profileImage}
+              profileName={guide.profileName}
+              experience={guide.experience}
+              review={guide.review}
+              facebookLink={guide.facebook}
+              instagramLink={guide.instagram}
+              chargeStatus={guide.chargestatus}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
