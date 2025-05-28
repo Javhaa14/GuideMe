7307cdf9e5f16fbd312753c1896e980b37ba1b49
24LP1634 24LP1634 <24LP1634@G302-CC0108.local>
2025-05-28 11:21:29 +0800

    lol

--- front/app/(main)/Guidesinfo/components/Filter.tsx
+++ front/app/(main)/Guidesinfo/components/Filter.tsx
@@ -1,6 +1,20 @@
-export const Filter = ({ name }: { name: string }) => {
+export const Filter = ({
+  name,
+  onclick,
+  index,
+  clicked,
+}: {
+  name: string;
+  onclick: (index: number) => void;
+  index: number;
+  clicked: boolean;
+}) => {
   return (
-    <button className="flex p-3 w-fit h-fit rounded-md border-[1px] border-black text-[16px] text-black">
+    <button
+      onClick={() => onclick(index)}
+      className={`flex p-3 w-fit h-fit rounded-md border-[1px] border-black text-[16px] hover:bg-black text-black hover:text-white ${
+        clicked && "bg-black text-white"
+      }`}>
       {name}
     </button>
   );
