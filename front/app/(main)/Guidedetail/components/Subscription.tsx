c88613d3b021cd038dfa6dd9efb205e3fe255453
Javkhlan Battulga <Javkhlan.B@ckbank.mn>
2025-05-29 19:09:20 +0800

    qpay subscription

--- front/app/(main)/Guidedetail/components/Subscription.tsx
+++ front/app/(main)/Guidedetail/components/Subscription.tsx
@@ -8,7 +8,49 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Card } from "./Card";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export const Subscription = () => {
+  const [step, setStep] = useState(1);
+  const [paymentId, setPaymentId] = useState(null);
+  const [status, setStatus] = useState("");
+
+  const [qr, setQr] = useState("");
+  const handleplan = async (e: any, plan: string | null) => {
+    e.stopPropagation();
+
+    if (!plan) return;
+
+    setStep(2);
+    setQr("");
+    setPaymentId(null);
+    setStatus("");
+
+    // You can send the selected plan to your backend if needed
+    const data = await axios.get(`https://guideme-8o9f.onrender.com`, {
+      params: { plan }, // send selected plan to backend (optional)
+    });
+
+    setQr(data.data.qr);
+    setPaymentId(data.data.id);
+  };
+
+  useEffect(() => {
+    if (!paymentId) return;
+
+    const ws = new WebSocket("https://guideme-8o9f.onrender.com");
+    ws.onopen = () => {
+      ws.send(JSON.stringify({ type: "watch", paymentId }));
+    };
+    ws.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      if (message.status === true) {
+        setStatus("payment success");
+        ws.close();
+      }
+    };
+  }, [paymentId]);
   return (
     <Dialog>
       <DialogTrigger>
@@ -16,12 +58,13 @@ export const Subscription = () => {
           Subscription
         </span>
       </DialogTrigger>
-      <DialogContent className="w-fit">
+      <DialogContent className="w-fit flex justify-center">
         <DialogHeader>
           <DialogTitle></DialogTitle>
-          <DialogDescription className="flex w-fit flex-col justify-center items-center">
-            <Card />
-          </DialogDescription>
+          <div className="flex w-fit flex-col justify-center items-center">
+            {" "}
+            <Card handleplan={handleplan} step={step} qr={qr} status={status} />
+          </div>
         </DialogHeader>
       </DialogContent>
     </Dialog>
