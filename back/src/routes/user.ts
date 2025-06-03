a138f49c5a0d0350572f94d2e962e551b041bf35
Javkhlan Battulga <Javkhlan.B@ckbank.mn>
2025-06-03 18:01:34 +0800

    zasvar

---
+++ back/src/routes/user.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import {
+  createUser,
+  deleteUserById,
+  getUserById,
+  getUsers,
+  updateUserById,
+} from "../controller/user";
+
+export const userRouter = Router();
+
+userRouter
+  .post("/", createUser as any)
+  .get("/", getUsers as any)
+  .get("/", getUserById as any)
+  .delete("/:id", deleteUserById as any)
+  .put("/:id", updateUserById as any);
