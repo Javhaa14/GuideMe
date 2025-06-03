451b0799a88947f4416d38c1bd43b3d911e06cd9
Javkhlan Battulga <Javkhlan.B@ckbank.mn>
2025-06-03 17:21:36 +0800

    fix

--- back/src/routes/post.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createPost, getPosts, getPostsByTouristId } from "../controller/post";
-
-export const postRouter = express.Router();
-
-postRouter
-  .post("/", createPost)
-  .get("/", getPosts)
-  .get("/:touristId", getPostsByTouristId);
