d0e79b23c93f637afe2e7ca0dd4b2d29f439847d
24LP1634 24LP1634 <24LP1634@G302-CC0108.local>
2025-06-04 12:21:58 +0800

    pushhh

--- front/app/(main)/touristProfile/page.tsx
+++ front/app/(main)/touristProfile/page.tsx
@@ -5,9 +5,8 @@ import TravelerMainProfile from "./components/TravelerMainProfile";
 const page = () => {
   return (
     <div className="flex justify-center px-[200px] py-[50px]">
-      {" "}
-      {/* <TouristProfile /> */}
-      <TravelerMainProfile />
+      <TouristProfile />
+      {/* <TravelerMainProfile /> */}
     </div>
   );
 };
