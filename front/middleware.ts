bcabb59c82aa89d87cc8cc13d36b502f51310cda
24LP1634 24LP1634 <24LP1634@G302-CC0108.local>
2025-06-06 13:37:00 +0800

    ll

--- front/middleware.ts
+++ front/middleware.ts
@@ -1,46 +1,37 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// export function middleware(request: NextRequest) {
-//   const token = request.cookies.get("token")?.value;
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
 
-//   // List of protected paths
-//   const protectedPaths = [
-//     "/Guidedetail",
-//     "/guideProfile",
-//     "/Guidesinfo",
-//     "/notification",
-//     "/Settings",
-//     "/Touristdetail",
-//     "/touristProfile",
-//     "/Travelersinfo",
-//   ];
+  // List of protected paths
+  const protectedPaths = [
+    "/Guidedetail",
+    "/guideProfile",
+    "/Guidesinfo",
+    "/notification",
+    "/Settings",
+    "/Touristdetail",
+    "/touristProfile",
+    "/Travelersinfo",
+  ];
 
-//   const isProtectedPath = protectedPaths.some((path) =>
-//     request.nextUrl.pathname.startsWith(path)
-//   );
+  const isProtectedPath = protectedPaths.some((path) =>
+    request.nextUrl.pathname.startsWith(path)
+  );
 
-//   if (isProtectedPath) {
-//     if (!token) {
-//       // Redirect to login if no token
-//       const loginUrl = new URL("/log-in", request.url);
-//       loginUrl.searchParams.set("from", request.nextUrl.pathname);
-//       return NextResponse.redirect(loginUrl);
-//     }
-//   }
+  if (isProtectedPath) {
+    if (!token) {
+      // Redirect to login if no token
+      const loginUrl = new URL("/log-in", request.url);
+      loginUrl.searchParams.set("from", request.nextUrl.pathname);
+      return NextResponse.redirect(loginUrl);
+    }
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: [
-//     "/Guidedetail/:path*",
-//     "/guideProfile/:path*",
-//     "/Guidesinfo/:path*",
-//     "/notification/:path*",
-//     "/Settings/:path*",
-//     "/Touristdetail/:path*",
-//     "/touristProfile/:path*",
-//     "/Travelersinfo/:path*",
-//   ],
-// };
+export const config = {
+  matcher: ["/Guidedetail/:path*"],
+};
