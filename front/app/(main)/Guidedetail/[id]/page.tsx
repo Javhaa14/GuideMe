8d255a23d835954c3fffafc4b18b0b2a0ce76b55
24LP1634 24LP1634 <24LP1634@G302-CC0108.local>
2025-06-06 10:26:43 +0800

    l

--- front/app/(main)/Guidedetail/[id]/page.tsx
+++ front/app/(main)/Guidedetail/[id]/page.tsx
@@ -1,7 +1,6 @@
 import { Footer } from "../../components/Footer";
 import GuideMainProfile from "../components/GuideMainProfile";
 import { GuideProfileNav } from "../components/GuideProfileNav";
-import TouristSee from "../components/TouristSee";
 
 export default function Home() {
   return (
