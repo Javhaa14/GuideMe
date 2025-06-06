c1bf0b4a7eeeedebca5f339374e8ccd30012f08e
zayabudmir <zayabudmir@gmail.com>
2025-06-06 10:05:49 +0800

    hi

--- front/app/(auth)/forget-pass/page.tsx
+++ front/app/(auth)/forget-pass/page.tsx
@@ -4,8 +4,8 @@ import { VerifyEmail } from "./component/VerifyEmail";
 export default function Home() {
   return (
     <div>
-      {/* <PassPage /> */}
-      <VerifyEmail />
+      <PassPage />
+      {/* <VerifyEmail /> */}
     </div>
   );
 }
