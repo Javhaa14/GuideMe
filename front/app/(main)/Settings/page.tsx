c0b94a759d58c87dc7ba499292d12120b8ba2000
zayabudmir <zayabudmir@gmail.com>
2025-06-06 09:51:48 +0800

    hello

--- front/app/(main)/Settings/page.tsx
+++ front/app/(main)/Settings/page.tsx
@@ -4,8 +4,6 @@ import { useSearchParams } from "next/navigation";
 import { SettingsButton } from "./components/SettingsButton";
 import { Password } from "./components/Password";
 import { Personal } from "./components/Personal";
-import { SettingsNav } from "./components/SettingsNav";
-import { Footer } from "../components/Footer";
 
 export default function Page() {
   const searchParams = useSearchParams();
@@ -13,10 +11,8 @@ export default function Page() {
 
   return (
     <div className="space-y-6">
-      <SettingsNav />
       <SettingsButton />
       {tab === "password" ? <Password /> : <Personal />}
-      <Footer />
     </div>
   );
 }
