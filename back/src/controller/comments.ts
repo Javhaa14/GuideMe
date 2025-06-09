54c6449cf7fcb85b4a0a8e7f7029bbcef108e4fd
24LP1634 24LP1634 <24LP1634@G302-CC0121.local>
2025-06-09 09:17:45 +0800

    save

--- back/src/controller/comments.ts
+++ back/src/controller/comments.ts
@@ -15,12 +15,14 @@ export const createComment = async (
       review,
       recommend,
     });
-
-    await Guidemodel.findByIdAndUpdate(
-      userId,
-      { $push: { comments: comment._id } },
-      { new: true }
-    );
+    const guide = await Guidemodel.findById(userId);
+    if (!guide) {
+      console.error("Guide not found with ID:", userId);
+    } else {
+      guide.comments.push(comment._id);
+      await guide.save();
+      console.log("Comment pushed and guide saved:", guide);
+    }
 
     res.status(200).send({
       success: true,
