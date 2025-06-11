00001aa5310b2f3dd7c3524cb5aa5302f32203e1
24LP1634 24LP1634 <24LP1634@G302-CC0108.local>
2025-06-11 12:51:36 +0800

    o

--- back/src/routes/auth.ts
+++ back/src/routes/auth.ts
@@ -2,6 +2,10 @@ import { Router } from "express";
 import { checkUsername, signup } from "../controller/auth/sign-up.controller";
 import { signin } from "../controller/auth/sign-in.controller";
 import { checkOrCreateUser } from "../controller/sda";
+import {
+  requestReset,
+  verifyResetToken,
+} from "../controller/auth/reset-password";
 
 export const authRouter = Router();
 
@@ -9,4 +13,6 @@ authRouter
   .post("/check-user", checkUsername)
   .post("/signup", signup)
   .post("/signin", signin)
-  .post("/check-or-create-user", checkOrCreateUser);
+  .post("/check-or-create-user", checkOrCreateUser)
+  .post("/request-reset", requestReset)
+  .post("/verify-reset-token", verifyResetToken);
