8a31bb64b0d2f60d3261c4202d823948841a07d3
zayabudmir <zayabudmir@gmail.com>
2025-06-18 11:49:05 +0800

    hi

--- front/app/(main)/wish/page.tsx
+++ front/app/(main)/wish/page.tsx
@@ -1,9 +1,22 @@
+"use client";
+
 import WishlistCard from "./components/WishlistCard";
 
 export default function Home() {
   return (
-    <div className="px-80 py-20">
-      <WishlistCard />
+    <div className="px-10 py-10">
+      <WishlistCard
+        id="1"
+        name="Gobi Desert Adventure"
+        location="Gobi, Mongolia"
+        image="/lake.png"
+        startDate="2025-07-15"
+        groupSize="small"
+        price={1200}
+        currency="USD"
+        isFavorite={true}
+        onRemove={() => alert("Removed from wishlist")}
+      />
     </div>
   );
 }
